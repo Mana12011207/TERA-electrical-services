1d51a6680d3bce75ce1f1bacd238ba17c5c86790
Mana Misumi <mana.misumi3050@gmail.com>
2024-11-07 22:05:11 +1100

    Add Services to routes in App.jsx

--- src/App.js
+++ src/App.js
@@ -3,13 +3,14 @@ import { Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import About from "./pages/About";
 import Contact from "./pages/Contact";
-import Navbar from "./components/Navbar";
+import Services from "./pages/Services";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/" element={<About />} />
+      <Route path="/" element={<Services />} />
       <Route path="/" element={<Contact />} />
     </Routes>
   );
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,8 @@
 import React from "react";
+import Navbar from "../components/Navbar";
 
-function Home() {}
+function Home() {
+  <Navbar />;
+}
 
 export default Home;
