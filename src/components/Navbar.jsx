a856c972fb483e013802757d2e03e78f2a52a79f
Mana Misumi <mana.misumi3050@gmail.com>
2024-11-08 11:43:32 +1100

    Applied custom colors and fonts to the entire application

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,17 +1,17 @@
 import React from "react";
 
 function Navbar() {
-  // return (
-  //   // <nav id="navbar">
-  //   //   <ul>
-  //   //     <img src="/assets/images/TeraElectricalServicesLogo.png" alt="" />
-  //   //     <li>Home</li>
-  //   //     <li>About</li>
-  //   //     <li>Services</li>
-  //   //     <li>Contact</li>
-  //   //   </ul>
-  //   // </nav>
-  // // );
+  return (
+    <nav className="flex flex-direction: row">
+      <ul>
+        <img src="/assets/images/TeraElectricalServicesLogo.png" alt="" />
+        <li>Home</li>
+        <li>About</li>
+        <li>Services</li>
+        <li>Contact</li>
+      </ul>
+    </nav>
+  );
 }
 
 export default Navbar;
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,25 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: {
+          default: "#0a6acb",
+        },
+        secondaryPrimary: {
+          default: "#ebebeb",
+        },
+        accent: {
+          default: "#f26419",
+        },
+        secondaryAcctent: {
+          DEFAULT: "#f6a02d",
+        },
+      },
+      fontFamily: {
+        sans: ["Merriweather", "sans-serif"],
+      },
+    },
   },
   plugins: [],
 };
