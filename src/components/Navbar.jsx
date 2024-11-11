5fa5c56c102de15587430b65be1dd18371de3937
Mana Misumi <mana.misumi3050@gmail.com>
2024-11-12 09:57:58 +1100

    Implement basic hamburger menu functionality

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,20 +6,34 @@ function Navbar() {
   const [isVisible, setIsVisible] = useState(false);
 
   const openHamburgerMenue = () => {
+    setIsVisible(!isVisible);
     console.log("メニューを開きました");
   };
 
   return (
-    <nav className="flex justify-between items-center w-full pt-2 px-4">
-      <img
-        className="max-h-8"
-        src="/assets/images/TeraElectricalServicesLogo.png"
-        alt="logo"
-      ></img>
-      <button onClick={openHamburgerMenue}>
-        <GiHamburgerMenu className="text-xl" />
-      </button>
-    </nav>
+    <>
+      <div className="flex justify-between items-center w-full pt-2 px-4">
+        <img
+          className="max-h-8"
+          src="/assets/images/TeraElectricalServicesLogo.png"
+          alt="logo"
+        ></img>
+        <button onClick={openHamburgerMenue}>
+          <GiHamburgerMenu className="text-xl" />
+        </button>
+        {isVisible && (
+          <nav>
+            <ul>
+              <li>Home</li>
+              <li>Services</li>
+              <li>About</li>
+              <li>Contact</li>
+            </ul>
+          </nav>
+        )}
+      </div>
+      <nav></nav>
+    </>
   );
 }
 
