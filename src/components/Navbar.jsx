00377e553acbdc3d41535a68d41e565927f70893
Mana Misumi <mana.misumi3050@gmail.com>
2024-11-14 21:52:47 +1100

    Fix issue with button displaying the wrong icon

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { useState } from "react";
 import { GiHamburgerMenu } from "react-icons/gi";
-import { IoMdClose } from "react-icons/io";
+import { GrFormClose } from "react-icons/gr";
 
 function Navbar() {
   const [isVisible, setIsVisible] = useState(false);
@@ -13,43 +13,49 @@ function Navbar() {
 
   return (
     <>
-      <div className="flex justify-between items-center w-full pt-2 px-4 ">
+      <div className="flex justify-between items-center w-full pt-2 px-4 relative">
+        <button
+          className="w-8 h-8  bg-white  bg-opacity-40 rounded-full justify-items-center cursor-pointer"
+          onClick={toggleHamburgerMenue}
+        >
+          {isVisible ? <GrFormClose /> : <GiHamburgerMenu />}
+        </button>
         <img
           className="max-h-8"
           src="/assets/images/TeraElectricalServicesLogo.png"
           alt="logo"
         ></img>
-        <button onClick={toggleHamburgerMenue}>
-          <GiHamburgerMenu className="text-xl " />
-        </button>
       </div>
       <div
-        className={`fixed top-0 left-0 w-full bg-red-100 py-8 pl-5 transform transition-transform duration-300 ${
+        className={`fixed top-0 left-0 w-full bg-primary-default bg-opacity-50 py-8 pl-5 transform transition-transform duration-300 ${
           isVisible ? "translate-x-0" : "-translate-x-full"
         }`}
       >
         <nav>
-          <button onClick={toggleHamburgerMenue} className="text-xl mb-4">
-            <IoMdClose />
-          </button>
-          <ul className="space-y-2">
+          <ul className="space-y-2 cursor-pointer">
             <li>
-              <a href="#home" className="hover:text-blue-500">
+              <a href="/" className="hover:text-secondaryPrimary-default ">
                 Home
               </a>
-            </li>{" "}
+            </li>
             <li>
-              <a href="#services" className="hover:text-blue-500">
+              <a
+                href="/services"
+                className="hover:text-secondaryPrimary-default "
+              >
                 Services
               </a>
             </li>
             <li>
-              <a href="#about" className="hover:text-blue-500">
+              <a href="/about" className="hover:text-secondaryPrimary-default">
                 About
               </a>
             </li>
             <li>
-              <a href="#contact" className="hover:text-blue-500">
+              <a
+                href="/contact"
+                className="hover:text-secondaryPrimary-default "
+              >
                 Contact
               </a>
             </li>{" "}
