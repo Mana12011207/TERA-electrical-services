1249e7c2fa30c1ff938f2458f06b17d9bdc28d97
Mana Misumi <mana.misumi3050@gmail.com>
2024-11-14 12:28:33 +1100

    Add close button to hamburger menu

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { useState } from "react";
 import { GiHamburgerMenu } from "react-icons/gi";
+import { IoMdClose } from "react-icons/io";
 
 function Navbar() {
   const [isVisible, setIsVisible] = useState(false);
@@ -22,14 +23,19 @@ function Navbar() {
           <GiHamburgerMenu className="text-xl " />
         </button>
         {isVisible && (
-          <nav className="fixed bg-red-100 left-0 top-0 w-full py-8 pl-5">
-            <ul className="space-y-2">
-              <li>Home</li>
-              <li>Services</li>
-              <li>About</li>
-              <li>Contact</li>
-            </ul>
-          </nav>
+          <div className="fixed top-0 left-0 bg-red-100   w-full py-8 pl-5 transform transition-transform duration-300 ${isVisible ? 'translate-x-0' :">
+            <nav>
+              <button onClick={toggleHamburgerMenue}>
+                <IoMdClose className="text-xl" />
+              </button>
+              <ul className="space-y-2">
+                <li>Home</li>
+                <li>Services</li>
+                <li>About</li>
+                <li>Contact</li>
+              </ul>
+            </nav>
+          </div>
         )}
       </div>
       <nav></nav>
