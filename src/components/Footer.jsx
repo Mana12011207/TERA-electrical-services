3045699ee55fb456c7b621093af0a5eef482b464
Mana Misumi <mana.misumi3050@gmail.com>
2024-11-26 09:51:19 +1100

    Initial setup for Footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 
-function Footer() {}
-
+function Footer() {
+  return <></>;
+}
 export default Footer;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -14,10 +14,12 @@ function Navbar() {
 
   return (
     <>
-      <div className="flex justify-between w-full h-full">
+      {/* Collapsed Hamburger */}
+      <div className="w-full h-full lg:grid lg:grid-cols-3 lg:gap-4">
         <button
-          className="z-10 w-8 h-8 mt-2 ml-2 bg-white rounded-full cursor-pointer bg-opacity-40 justify-items-center md:hidden shrink-0"
+          className="z-10 w-8 h-8 mt-2 ml-2 bg-white rounded-full cursor-pointer bg-opacity-40 justify-items-center lg:hidden shrink-0"
           onClick={toggleHamburgerMenu}
+          aria-label="Toggle menu"
         >
           {isMenuOpen ? (
             <GrFormClose className="text-lg" />
@@ -25,15 +27,17 @@ function Navbar() {
             <GiHamburgerMenu />
           )}
         </button>
+
+        {/* Branding Image */}
         <img
-          className="box-border w-auto h-auto mt-2 ml-2 max-h-8 sm:max-h-36 xl:ml-32"
-          src="assets/images/Tera-electrical-services.png"
+          className="box-border w-auto h-auto max-h-8 lg:col-span-1"
+          src="assets/images/TeraElectricalServices.png"
           alt="logo"
         ></img>
 
         {/* Horizontal Menu for larger screens */}
-        <div className="hidden md:flex">
-          <nav className="w-auto md:flex md:items-end md:mx-2">
+        <div className="hidden lg:grid lg:col-span-1">
+          <nav className="w-auto lg:flex lg:items-end">
             <ul className="flex space-x-10 cursor-pointer sm:text-sm lg:text-xl xl:text-2xl">
               <li className="">
                 <a href="/">Home</a>
@@ -50,8 +54,9 @@ function Navbar() {
             </ul>
           </nav>
         </div>
+
         {/* CTA button */}
-        <button className="box-border flex-col items-center hidden h-auto mt-2 mr-2 font-bold rounded-lg md:flex -auto md:max-h-36 bg-secondaryAccent xl:mr-32 2xl:h-auto 2xl:w-80">
+        <button className="box-border flex-col items-center hidden w-auto h-auto font-bold rounded-lg lg:flex bg-secondaryAccent">
           <div className="p-4">
             <span className="border border-red-500">Call Now</span>
             <div className="flex items-center">
