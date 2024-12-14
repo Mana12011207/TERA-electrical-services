ced8887c680835b181bff201be57ff00a1dd199c
Mana Misumi <mana.misumi3050@gmail.com>
2024-12-15 08:20:16 +1100

    Add carousel to home page

--- src/components/Testimonials.jsx
+++ src/components/Testimonials.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 const reviews = [
   {
@@ -29,23 +29,56 @@ const reviews = [
 ];
 
 function Testimonials() {
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  const nextSlide = () => {
+    setCurrentIndex((prevIndex) => (prevIndex + 1) % reviews.length);
+  };
+
+  const prevSlide = () => {
+    setCurrentIndex(
+      (prevIndex) => (prevIndex - 1 + reviews.length) % reviews.length
+    );
+  };
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      nextSlide();
+    }, 3000);
+
+    return () => clearInterval(interval);
+  }, []);
+
   return (
     <>
-      <div className="flex flex-col items-center">
-        <p> What Our Clients Say About Us</p>
+      <div className="flex flex-col items-center min-h-screen p-8 bg-gray-100">
+        <p className="mb-4 text-2xl font-bold">
+          {" "}
+          What Our Clients Say About Us
+        </p>
         <img
           src="assets/images/TestimonyStars.svg"
           alt="Testimonials Star"
+          className="mb-6"
         ></img>
 
-        {reviews.map((review, index) => (
-          <div key={index} className="flex items-center flex-cols-1">
-            <div className="flex-col p-6 bg-yellow-500 rounded-lg shadow-md ">
-              <h3>{review.review}</h3>
-              <p className="font-bold text-center">{review.name}</p>
+        <div className="relative w-full max-w-xl">
+          <div className="overflow-hidden">
+            <div
+              className="flex transition-transform duration-500 ease-in-out"
+              style={{ transform: `translateX(-${currentIndex * 100}%)` }}
+            >
+              {reviews.map((review, index) => (
+                <div key={index} clas className="flex-shrink-0 w-full p-4">
+                  <div className="flex flex-col p-6 bg-yellow-500 rounded-lg shadow-md ">
+                    <h3>{review.review}</h3>
+                    <p className="font-bold text-center">{review.name}</p>
+                  </div>
+                </div>
+              ))}
             </div>
           </div>
-        ))}
+        </div>
       </div>
     </>
   );
