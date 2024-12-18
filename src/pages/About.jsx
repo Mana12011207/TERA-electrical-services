b2e2fb01962cee0d5fe8ccc7e59606f768098498
Mana Misumi <mana.misumi3050@gmail.com>
2024-12-18 20:55:05 +1100

    Adjust hero image height based on screen size

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -4,12 +4,26 @@ import BaseLayout from "../components/BaseLayout";
 function About() {
   return (
     <BaseLayout>
-      <div className="w-full">
-        <img
-          src="assets/images/AboutUS.svg"
-          alt="About us"
-          className="w-full"
-        />
+      <div className="flex flex-col">
+        <div className="w-full">
+          <img
+            src="assets/images/AboutUS.svg"
+            alt="About us"
+            className="object-cover w-full h-32"
+          />
+        </div>
+        <p>
+          With over 15 years of hands-on experience in electrical services, We
+          bring a wealth of expertise and commitment to quality and safety in
+          every project. Our journey as an electrician has equipped us with the
+          skills and knowledge to handle a wide range of electrical needs, from
+          residential repairs to complex commercial installations. We believe in
+          delivering efficient, reliable solutions tailored to each client’s
+          unique requirements. Our dedication to staying updated with industry
+          standards ensures that every job is done right the first time,
+          providing peace of mind and long-lasting results.
+        </p>
+        <h2>FAQs</h2>
       </div>
     </BaseLayout>
   );
