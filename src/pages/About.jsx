0b895ffe962b970c02093eb10fdca34e718500e2
Mana Misumi <mana.misumi3050@gmail.com>
2024-12-18 20:39:12 +1100

    Add a hero image to the about us page

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,7 +1,18 @@
 import React from "react";
+import BaseLayout from "../components/BaseLayout";
 
 function About() {
-  return <div>About</div>;
+  return (
+    <BaseLayout>
+      <div className="w-full">
+        <img
+          src="assets/images/AboutUS.svg"
+          alt="About us"
+          className="w-full"
+        />
+      </div>
+    </BaseLayout>
+  );
 }
 
 export default About;
