c64e0edd1e2420fd8bb6accf6ff6e8e35fd7ff88
Mana Misumi <mana.misumi3050@gmail.com>
2024-12-30 09:07:36 +1100

    Add content images to the services page

--- src/pages/Services.jsx
+++ src/pages/Services.jsx
@@ -15,7 +15,44 @@ function Services() {
             Our Services
           </p>
         </div>
-        <h1>Residential and Commercial</h1>
+        <h1 className="mt-8 text-lg text-center">Residential and Commercial</h1>
+        <div className="w-full px-10 py-8 mx-auto max-w-7xl">
+          <div>
+            <p className="text-center">Repair and Renewals</p>
+            <img
+              src="assets/images/RepairAndRenewals.svg"
+              alt="RepairAndRenewals"
+            />
+            <ul>
+              <li>Extensions</li>
+              <li>Switchboard Upgrades</li>
+              <li>New Sockets and more...</li>
+            </ul>
+          </div>
+          <div>
+            <p className="text-center">Installation</p>
+            <img src="assets/images/Installation.svg" alt="Installation" />
+            <ul>
+              <li>All types Lighting</li>
+              <li>Appliance Installation</li>
+              <li>Security System Installation</li>
+              <li>Network and Data Cabling and more...</li>
+            </ul>
+          </div>
+          <div>
+            <p className="text-center">Inspection and Testing</p>
+            <img
+              src="assets/images/InspectionAndTesting.svg"
+              alt="InspectionAndTesting"
+            />
+            <ul>
+              <li>Wiring Inspection</li>
+              <li>Insulation Resistance Testing</li>
+              <li>Electrical Load Testing</li>
+              <li>Fault finding and more...</li>
+            </ul>
+          </div>
+        </div>
       </div>
     </BaseLayout>
   );
