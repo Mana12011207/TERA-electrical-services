9aacd7f39bfa2043afa133d1c552e4843ccc108d
Mana Misumi <mana.misumi3050@gmail.com>
2025-01-03 20:43:54 +1100

    Add hero image to the contact page

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -5,7 +5,18 @@ function Contact() {
   return (
     <>
       <BaseLayout>
-        <h1>Contact US</h1>
+        <div className="flex flex-col items-center w-full">
+          <div className="relative w-full ">
+            <img
+              src="assets/images/ContactUs.svg"
+              alt="Contact us"
+              className="object-cover w-full h-32 md:min-h-64 "
+            />
+            <p className="absolute inset-0 flex items-center justify-center text-lg text-white bg-black bg-opacity-40 md:text-2xl">
+              Contact Us
+            </p>
+          </div>
+        </div>
       </BaseLayout>
     </>
   );
