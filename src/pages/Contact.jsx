139676bc0e4e7cd572d98d1119c40fb167d3b8c4
Mana Misumi <mana.misumi3050@gmail.com>
2025-01-06 14:39:02 +1100

    Add complete styling for the contact page

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -16,53 +16,103 @@ function Contact() {
               Contact Us
             </p>
           </div>
-          <div className="w-full px-10 py-8 mx-auto max-w-7xl">
-            <h2 className="text-2xl font-bold text-center lg:text-3xl">
-              Reach out TERA Electricians today
-            </h2>
-            <p className="mt-2 text-sm tracking-wide text-gray-500 md:text-xl">
-              Our expert electricians are ready to answer any questions,
-              schedule service, or provide estimates. Just leave us a message
-              below, and we’ll get back to you soon.
-            </p>
-            <div className="flex">
-              <form
-                action=""
-                method="get"
-                className="items-center mt-2 space-y-2 text-center"
-              >
-                <input
-                  type="text"
-                  name="first name"
-                  required
-                  placeholder="Full name"
-                  className="bg-yellow-500 border rounded-sm  bg-opacity-30 solid"
-                />
-                <input
-                  type="email"
-                  name="email"
-                  placeholder="Email"
-                  className="bg-yellow-500 border rounded-sm bg-opacity-30 solid"
-                ></input>
-                <input
-                  type="number"
-                  name="Phone"
-                  placeholder="Phone"
-                  className="bg-yellow-500 border rounded-sm bg-opacity-30 solid"
-                ></input>
-                <input
-                  type="text"
-                  name="address"
-                  placeholder="Home Address"
-                  className="bg-yellow-500 border rounded-sm bg-opacity-30 solid"
-                ></input>
-                <textarea
-                  name="message"
-                  placeholder="Message"
-                  className="bg-yellow-500 border rounded-sm bg-opacity-30 solid"
-                />
-                <input type="submit" className="border solid" />
-              </form>
+
+          <div className="flex flex-col items-center justify-center w-full px-10 py-8 mx-auto max-w-7xl">
+            <div className="w-full max-w-2xl px-10 py-8 mx-auto bg-gray-200 rounded-lg">
+              <h2 className="text-2xl font-bold text-center lg:text-3xl">
+                Reach out TERA Electricians today
+              </h2>
+              <p className="mt-2 text-sm tracking-wide text-center text-gray-500 md:text-xl">
+                Our expert electricians are ready to answer any questions,
+                schedule service, or provide estimates. <br /> Just leave us a
+                message below, and we’ll get back to you soon.
+              </p>
+
+              {/* Contac form */}
+              <div className="flex justify-center mt-6">
+                <form
+                  action=""
+                  method="get"
+                  className="items-center mt-2 space-y-4"
+                >
+                  <div className="flex flex-col items-start ">
+                    <label
+                      htmlFor="full-name"
+                      className="block text-sm font-bold text-gray-700"
+                    >
+                      Name
+                    </label>
+                    <input
+                      type="text"
+                      name="full name"
+                      required
+                      placeholder="Full name"
+                      className="p-1 bg-yellow-500 border rounded-sm bg-opacity-30 solid md:pr-20"
+                    />
+                  </div>
+                  <div className="flex flex-col items-start">
+                    <label
+                      htmlFor="full-name"
+                      className="block text-sm font-bold text-gray-700"
+                    >
+                      Email
+                    </label>
+                    <input
+                      type="email"
+                      name="email"
+                      placeholder="Email"
+                      className="p-1 bg-yellow-500 border rounded-sm bg-opacity-30 solid md:pr-20"
+                    ></input>
+                  </div>
+                  <div className="flex flex-col items-start">
+                    <label
+                      htmlFor="full-name"
+                      className="block text-sm font-bold text-gray-700"
+                    >
+                      Phone
+                    </label>
+                    <input
+                      type="number"
+                      name="Phone"
+                      placeholder="Phone"
+                      className="p-1 bg-yellow-500 border rounded-sm bg-opacity-30 solid md:pr-20"
+                    ></input>
+                  </div>
+                  <div className="flex flex-col items-start">
+                    <label
+                      htmlFor="full-name"
+                      className="block text-sm font-bold text-gray-700"
+                    >
+                      Address
+                    </label>
+                    <input
+                      type="text"
+                      name="address"
+                      placeholder="Home Address"
+                      className="p-1 bg-yellow-500 border rounded-sm bg-opacity-30 solid md:pr-20"
+                    ></input>
+                  </div>
+                  <div className="flex flex-col items-start">
+                    <label
+                      htmlFor="full-name"
+                      className="block text-sm font-bold text-gray-700"
+                    >
+                      Enter Your Message Here
+                    </label>
+                    <textarea
+                      name="message"
+                      placeholder="Message"
+                      className="p-1 pb-16 bg-yellow-500 border rounded-sm bg-opacity-30 solid md:pr-20"
+                    />
+                  </div>
+                  <div className="text-center">
+                    <input
+                      type="submit"
+                      className="p-2 bg-blue-500 border rounded-sm shadow-md bg-opacity-80 solid"
+                    />
+                  </div>
+                </form>
+              </div>
             </div>
           </div>
         </div>
